460bac20e6213dd583dc6f0cd07527a80aa40daf
AlexiSpaceTime <alexis.fernandez@genstudents.org>
2024-10-11 19:54:41 -0300

    ExpressLab

---
+++ client.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+
+const data = {
+    title: 'New Post Title',
+    content: 'New Post Content'
+};
+
+axios.post('http://localhost:3000/posts', data)
+    .then(response => {
+        console.log('Post created successfully:', response.data);
+    })
+    .catch(error => {
+        console.error('Error creating post:', error);
+    });
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.use(express.json());
+
+let posts = [];
+
+app.post('/posts', (req, res) => {
+    const { title, content } = req.body;
+
+    const newPost = {
+        id: posts.length + 1,
+        title: title,
+        content: content
+    };
+
+    posts.push(newPost);
+
+    res.status(201).json(newPost);
+});
+
+app.listen(port, () => {
+    console.log('Server running at http://localhost:3000');
+});
